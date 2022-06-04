92f5b41406424cdc1efd63640ee1c467197f0e0c
Pina Gomet <101363048+pgomet@users.noreply.github.com>
2022-06-04 02:00:51 -0400

    cleaned index

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require("path");
 
 const generateHTML = require('./src/generateHTML');
 
-// team profiles
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -166,33 +165,24 @@ const addEmployee = () => {
         }
     ])
     .then(employeeData => {
-        // data for employee types 
-
         let { name, id, email, role, github, school, confirmAddEmployee } = employeeData; 
         let employee; 
 
         if (role === "Engineer") {
             employee = new Engineer (name, id, email, github);
-
             console.log(employee);
-
         } else if (role === "Intern") {
             employee = new Intern (name, id, email, school);
-
             console.log(employee);
         }
-
         teamArray.push(employee); 
-
         if (confirmAddEmployee) {
             return addEmployee(teamArray); 
         } else {
             return teamArray;
         }
     })
-
 };
-
 
 // Create HTML
 const writeFile = data => {
